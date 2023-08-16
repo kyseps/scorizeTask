84d2e5e6ed2d03f0a35ce9776363edea62c7a4c2
kyseps <screamer971@gmail.com>
2023-08-16 14:16:41 +0330

    -

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,46 +9,50 @@ export default function Nav() {
   return (
     <nav className="bg-white rounded-xl">
       <div className="flex gap-3 items-center px-2">
-        <Button
-          variant="default"
-          className="bg-[#94166d] flex items-center gap-2 px-3 py-2 rounded-xl text-white"
-        >
-          ورود / ثبت نام
-          <PersonIcon />
-        </Button>
+        <div className="flex gap-3 items-center">
+          <Button
+            variant="default"
+            className="bg-[#94166d] flex items-center gap-2 px-3 py-2 rounded-xl text-white"
+          >
+            ورود / ثبت نام
+            <PersonIcon />
+          </Button>
 
-        <Link className="p-3 border-solid border border-[#94166d] rounded-xl">
-          <LockClosedIcon className="text-[#94166d]" />
-        </Link>
-
-        <div className="flex gap-7 ml-[357px]">
-          <NavLink className="py-[8px]" to="blog">
-            بلاگ
-          </NavLink>
-          <NavLink className="py-[8px]" to="about">
-            درباره ما
-          </NavLink>
-          <NavLink className="py-[8px]" to="achievements">
-            موفقیت ها
-          </NavLink>
-          <NavLink className="py-[8px]" to="/">
-            رزرو مشاوره
-          </NavLink>
+          <Link className="p-3 border-solid border border-[#94166d] rounded-xl">
+            <LockClosedIcon className="text-[#94166d]" />
+          </Link>
         </div>
-        <Separator className="bg-[#94166d] h-5" orientation="vertical" />
 
-        <Link to="/">
-          <div className="flex gap-2 items-center">
-            <span>
-              SCO<span className="text-[#94166d]">RIZE</span>
-            </span>
-            <Separator className="bg-[#94166d] h-5" orientation="vertical" />
-            <span className="text-[#94166d]">
-              <span className="text-black">اسکو</span>
-              رایز
-            </span>
+        <div className=" flex items-center ml-auto gap-7">
+          <div className="flex gap-7 ml-[357px]">
+            <NavLink className="py-[8px]" to="blog">
+              بلاگ
+            </NavLink>
+            <NavLink className="py-[8px]" to="about">
+              درباره ما
+            </NavLink>
+            <NavLink className="py-[8px]" to="achievements">
+              موفقیت ها
+            </NavLink>
+            <NavLink className="py-[8px]" to="/">
+              رزرو مشاوره
+            </NavLink>
           </div>
-        </Link>
+          <Separator className="bg-[#94166d] h-5" orientation="vertical" />
+
+          <Link to="/">
+            <div className="flex gap-2 items-center">
+              <span>
+                SCO<span className="text-[#94166d]">RIZE</span>
+              </span>
+              <Separator className="bg-[#94166d] h-5" orientation="vertical" />
+              <span className="text-[#94166d]">
+                <span className="text-black">اسکو</span>
+                رایز
+              </span>
+            </div>
+          </Link>
+        </div>
       </div>
     </nav>
   );
